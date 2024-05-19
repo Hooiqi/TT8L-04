38fe9718322e9a21f3f1bc6c6e407c5414339232
Goh Qian Ying <qygoh0929@gmail.com>
2024-05-20 00:18:48 +0800

    add line to connect mysql

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname> #have not connected yet
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
