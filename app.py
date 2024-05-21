cda2b1077268e8d4bdf94cc3d41509161c89ddc2
Goh Qian Ying <qygoh0929@gmail.com>
2024-05-21 21:07:15 +0800

    change route to create_event.html

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname> #have not connected yet
-db = SQLAlchemy(app)
+#mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>
+#db = SQLAlchemy(app)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def create_event():
+    return render_template('create_event.html')
 
 if __name__ == "main":
     app.run(debug=True)
