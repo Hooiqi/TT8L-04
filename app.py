5202ef4a8abdad51dbc054f5324b91bf4d2869e0
Goh Qian Ying <qygoh0929@gmail.com>
2024-05-23 00:13:26 +0800

    connect database

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:tt8l_04@localhost/event_management_system'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def create_event():
