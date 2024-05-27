d8769ed988a8c7e3c73bf9e071d2ffb36ca11b51
Goh Qian Ying <qygoh0929@gmail.com>
2024-05-27 23:23:36 +0800

    import flaskwtf for forms

---
+++ forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, DateField, TimeField, DateTimeField, SelectField, FileField, FieldList, FormField, DecimalField, IntegerField
+from wtforms.validators import DataRequired, Length
+
+class TicketForm(FlaskForm):
+    ticket_type = StringField('Ticket Type', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[DataRequired()])
+    member_discount = DecimalField('Member Price', validators=[DataRequired()])
+    max_quantity = IntegerField('Quantity', validators=[DataRequired()])
+    start_sale = DateField('Start Sale', validators=[DataRequired()])
+    end_sale = DateField('End Sale', validators=[DataRequired()])
+
+class EventForm(FlaskForm):
+    event_name = StringField('Name', validators=[DataRequired()])
+    event_cat = SelectField('Category', choices=[('C01', 'Academic'), ('C02', 'Entertainments'), ('C03', 'Sports'), ('C04', 'Social'), ('C05', 'Others')], validators=[DataRequired()])
+    event_start = DateField('Start Date', validators=[DataRequired()])
+    event_end = DateField('End Date')
+    event_time = TimeField('Start Time', validators=[DataRequired()])
+    duration = StringField('Duration', validators=[DataRequired()])
+    event_img = FileField('Poster')
+    event_descr = TextAreaField('Description', validators=[DataRequired()])
+    event_venue = SelectField('Location type', choices=[('V01', 'On campus'), ('V02', 'Off campus'), ('V03', 'Online')], validators=[DataRequired()])
+    location_detail = StringField('Location details')
+    tickets = FieldList(FormField(TicketForm), min_entries=1)
