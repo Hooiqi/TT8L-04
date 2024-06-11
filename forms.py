427d5674407fe1077ea41b73558f79e9a8a11942
Goh Qian Ying <qygoh0929@gmail.com>
2024-06-11 21:12:04 +0800

    change validation, label text and placeholder

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, DateField, TimeField, DateTimeField, SelectField, PasswordField, FieldList, FormField, DecimalField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Optional, EqualTo, Length
+from wtforms.validators import DataRequired, ValidationError, Optional, EqualTo, Length, NumberRange, InputRequired
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask import flash
 from models import User
 
 class SignupForm(FlaskForm):
@@ -25,15 +26,15 @@ class SignupForm(FlaskForm):
         
 class TicketForm(FlaskForm):
     ticket_type = StringField('Ticket Type', validators=[DataRequired()], render_kw={"placeholder": "Ticket type*"})
-    price = DecimalField('Price', validators=[DataRequired()])
-    member_discount = DecimalField('Member Price', validators=[Optional()])
-    max_quantity = IntegerField('Quantity', validators=[DataRequired()])
+    price = DecimalField('Normal Price', validators=[InputRequired(), NumberRange(min=0)], render_kw={"placeholder": "Normal price*"})
+    member_discount = DecimalField('Member Price', validators=[InputRequired(), NumberRange(min=0)], render_kw={"placeholder": "Member price"})
+    max_quantity = IntegerField('Quantity', validators=[DataRequired()], render_kw={"placeholder": "Quantity*"})
     start_sale = DateField('Start Sale', validators=[DataRequired()])
     end_sale = DateField('End Sale', validators=[DataRequired()])
 
     def validate_end_sale(self, filed):
         if filed.data <= self.start_sale.data:
-            raise ValidationError('End sale date should be more than start sale date.')
+            raise ValidationError('Tickets end sale date should be later than start sale date')
 
 class EventForm(FlaskForm):
     event_name = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Event name*"})
@@ -42,13 +43,14 @@ class EventForm(FlaskForm):
     event_end = DateField('End Date', validators=[DataRequired()])
     event_time = TimeField('Start Time', validators=[DataRequired()])
     duration = StringField('Duration', validators=[DataRequired()], render_kw={"placeholder": "hour / minute *"})
-    event_img = FileField('Poster (max. file size 16MB)', validators=[FileRequired(), FileAllowed(['jpg','jpeg','png'])])
+    event_img = FileField('Poster', validators=[FileAllowed(['jpg','jpeg','png'], 'Image format only accepts jpeg, jpg or png')])
     event_descr = TextAreaField('Description', validators=[DataRequired()], render_kw={"placeholder": "Event details*"})
     event_venue = SelectField('Location type', choices=[('V01', 'On campus'), ('V02', 'Off campus'), ('V03', 'Online')], validators=[DataRequired()])
-    location_detail = StringField('Location details',  validators=[DataRequired()], render_kw={"placeholder": "Location details*"})
+    location_detail = StringField('Location/Platform',  validators=[DataRequired()], render_kw={"placeholder": "Details*"})
     tickets = FieldList(FormField(TicketForm), min_entries=1)
-    publish_status = StringField('Publish Status', validators=[DataRequired()])
+    publish_status = StringField('Publish Status')
 
     def validate_event_end(self, filed):
         if filed.data < self.event_start.data:
-            raise ValidationError('Event end date should be more than or equal to start date.')
+            raise ValidationError('Event end date should be later than or the same as start date')
+        
